a5f0cac1220e1c91958ba61ac532a4a5a87a1c93
Megan Shaw <meganrose@Megans-MacBook-Air.local>
2020-01-15 09:03:46 -0500

    Adding modal to frontend

--- public/js/index.js
+++ public/js/index.js
@@ -4,6 +4,7 @@
     $('.sidenav').sidenav();
     $('select').formSelect();
     $('.collapsible').collapsible();
+    $('.modal').modal();
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
